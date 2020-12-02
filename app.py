d0b0fb6ba9424cdea14483a3c4f2ac66bbb2b1b6
ndamukongjeremy <ndamukongjeremy@gmail.com>
2020-12-02 18:20:47 +0300

    fully implemented all given requirements

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'martdevelopers_flask_starter'
+app.config['MYSQL_DB'] = 'ROS'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # init MYSQL
 mysql = MySQL(app)
@@ -114,12 +114,6 @@ def is_logged_in(f):
     return wrap
 
 
-# Home 
-@app.route('/home')
-@is_logged_in
-def dashboard():
-    return render_template('home.html')
-
 
 # Logout
 @app.route('/logout')
@@ -129,6 +123,95 @@ def logout():
     flash('You are now logged out', 'success')
     return redirect(url_for('login'))
 
+# Home
+@app.route('/home')
+@is_logged_in
+def dashboard():
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get Loaded Orders
+    result = cur.execute("SELECT * FROM customer_orders ORDER BY created_at ASC ")
+    Responses = cur.fetchall()
+    if result > 0:
+        return render_template('home.html', Responses=Responses)      
+    else:
+        msg = 'No Available Customer Orders Available'
+        return render_template('home.html', msg=msg)
+    # Close connection
+    cur.close()
+
+#Get All Customer Orders
+def customerOrders():
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get Customer Orders
+    result = cur.execute("SELECT * FROM customer_orders ORDER BY created_at ASC")
+
+    customerOrders = cur.fetchall()
+
+    if result > 0:
+        return render_template('home.html', customerOrders=customerOrders)
+    else:
+        msg = 'No Available Customer Orders Available'
+        return render_template('home.html', msg=msg)
+    # Close connection
+    cur.close()
+
+#Order Form Helper Class
+class OrderForm(Form):
+    customer_name= StringField('Customer Name', [validators.length(min=1, max=250)])
+    order_food= StringField('Food Ordered', [validators.Length(min=1, max=200)])
+    qty = StringField('Quantity Ordered', [validators.length(min=1, max=200)])
+    price = StringField('Food Price',[validators.length(min=1,max=15)])
+    status = StringField('Order Payment Status',[validators.length(min=1,max=15)])
+
+#Make Order
+@app.route('/make_order', methods=['GET', 'POST'])
+@is_logged_in
+def add_Customer_Order():
+    form = OrderForm(request.form)
+    if request.method == 'POST' and form.validate():
+        order_food = form.order_food.data
+        qty = form.qty.data
+        price = form.price.data
+        status = form.status.data
+        customer_name = form.customer_name.data
+
+        # Create Cursor
+        cur = mysql.connection.cursor()
+
+        # Execute
+        cur.execute("INSERT INTO customer_orders(customer_name, order_food, qty, price, status) VALUES(%s, %s, %s, %s, %s)", (customer_name,order_food, qty, price, status))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        #Close connection
+        cur.close()
+
+        flash('Customer Order Submitted', 'success')
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('maker_order.html', form=form)
+
+
+#Pay Order -To Be Implemented Further
+
+#Print Receipt
+@app.route('/print_receipt/<string:id>/')
+def printReceipt(id):
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get Question
+    result = cur.execute("SELECT * FROM customer_orders WHERE id = %s", [id])
+
+    printReceipt = cur.fetchone()
+
+    return render_template('print_receipt.html', printReceipt=printReceipt)
 
 if __name__ == '__main__':
     app.secret_key = 'secret123'
